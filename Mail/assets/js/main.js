c218d34ad78914531af7fffa31a3b9a65ad5eb56
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-17 15:39:11 +0100

    Working on js loop

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -4,5 +4,15 @@ var btnEmail = document.getElementById("email_button");
 btnEmail.addEventListener("click", function () {
   var emailInput = document.getElementById("email");
   var email = emailInput.value;
-  console.log(emailInput);
+  console.log(email);
 })
+
+// Lista email
+var emailList = [
+  "andrea.romani@gmail.com","veronica.rossi@gmail.com","luca.affreschi@yahoo.com","mirko.rinelli@gmail.com","greta.ronchi@live.it","ettore.bertucci@libero.it","filippofalchi@gmail.com"
+]
+console.log(emailList);
+
+for (var i=6; i < emailList.length; i++) {
+  console.log(emailList[i]);
+}
